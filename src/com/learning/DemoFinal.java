a6e28d7aa0bd078a50a39cbdd7e701a7a3ef3d8e
ThanhHuong98 <ttinhoc15@gmail.com>
2019-07-10 16:59:13 +0700

    [u] variable && operator && final demo

---
+++ src/com/learning/DemoFinal.java
@@ -0,0 +1,9 @@
+package com.learning;
+
+public class DemoFinal {
+	public static void main (String[] args) {
+		final double PI = 3.1415926535897;
+		//PI = 3;
+		System.out.print(PI);
+	}
+}
---
+++ src/com/learning/DemoVariable.java
@@ -0,0 +1,22 @@
+package com.learning;
+
+public class DemoVariable {
+	public static void main(String[] args) {
+		
+		//declare variable
+		String name;
+		float height;
+		int age;
+		
+		//assign values ​​to variables
+		name = "Chau";
+		height = 1.7f;
+		age = 16;
+		
+		//Print all info on the console
+		System.out.println(name);
+		System.out.println(height);
+		System.out.println(age);
+		
+	}
+}
